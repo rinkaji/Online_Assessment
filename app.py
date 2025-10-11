5ed39ca456c68126285cf08e4af7c20ed1206b24
rinkaji <rinkajhin@gmail.com>
2025-10-11 18:18:51 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def old():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == "GET":
+        return render_template("login_page.html")
+    
+    if request.method == "POST":
+        return "login"
+
+@app.route('/home')
+def home():
+    return render_template("show_classes_page.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
