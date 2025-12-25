8533181348d2d5ebacb71fc5ea787f7cb4ae57f7
rinkaji <rinkajhin@gmail.com>
2025-12-25 12:52:43 +0800

    fixed the instructor side of admin routes

--- app/routes/adminRoutes.py
+++ app/routes/adminRoutes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for
-#import app.controllers.adminController as adminController
+from flask import Blueprint, flash, render_template, request, session, redirect, url_for
+from app.models import adminModel, instructorModel, studentModel
 
 
 admin_bp = Blueprint('admin', __name__)
@@ -15,19 +15,28 @@ def home():
 def getStudents():
     if not session:
         return redirect(url_for('instructor.login'))
-    return adminController.getStudents()
+    students = adminModel.getAllUsers('student')
+    return render_template('adminTemplate/admin_students_list.html', students = students)
+
 @admin_bp.route('/student/register', methods=["GET", 'POST'])
 def addStudent():
     if not session:
         return redirect(url_for('instructor.login'))
     if request.method == "POST":
-        return adminController.insertStudent(request.form)
+        student = studentModel.find_account(request.form.get('email',''))
+        if student:
+            flash("account already exists", 'error')
+            return redirect(url_for('admin.addStudent'))
+        studentModel.insert_student(request.form)
+        return redirect(url_for('admin.getStudents'))
     return render_template('adminTemplate/admin_student_registration.html')
+
 @admin_bp.route('/student/delete/<int:id>', methods= ['GET'])
 def removeStudent(id):
     if not session:
         return redirect(url_for('instructor.login'))
-    return adminController.deleteUser(id, 'student')
+    adminModel.deleteUser(id)
+    return redirect(url_for('admin.getStudents'))
 
 
 ##instructors side
@@ -35,18 +44,27 @@ def removeStudent(id):
 def getInstructors():
     if not session:
         return redirect(url_for('instructor.login'))
-    return adminController.getInstructors()
+    instructors = adminModel.getAllUsers('instructor')
+    return render_template('adminTemplate/admin_instructors_list.html', instructors = instructors)
 
 @admin_bp.route('/instructor/register', methods=["GET", 'POST'])
 def addInstructor():
     if not session:
         return redirect(url_for('instructor.login'))
     if request.method == "POST":
-        return adminController.insertInstructor(request.form)
+        #return adminController.insertInstructor(request.form)
+        instructor = instructorModel.find_account(request.form.get('email',''))
+        if instructor:
+            flash("account already exists", 'error')
+            return redirect(url_for('admin.addInstructor'))
+        instructorModel.insert_instructor(request.form)
+        return redirect(url_for('admin.getInstructors'))
     return render_template('adminTemplate/admin_instructor_registration.html')
 
 @admin_bp.route('/instructor/delete/<int:id>', methods= ['GET'])
 def removeInstructor(id):
     if not session:
         return redirect(url_for('instructor.login'))
-    return adminController.deleteUser(id, 'instructor')
+    adminModel.deleteUser(id)
+    return redirect(url_for('admin.getInstructors'))
+ 
